42216bc03cef41daca3c047ed23556a2d0322946
Nikhil <nikpat1988@gmail.com>
2017-08-13 03:43:53 +0530

    js

--- mobile.php
+++ mobile.php
@@ -1,7 +1,11 @@
 <!doctype html>
 <html>
     <head>
-        <title>sketch.js &raquo; Basic Example</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Smart Slate</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="css/example.css">
         <link rel="stylesheet" href="fa/css/font-awesome.min.css"> 
         <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
